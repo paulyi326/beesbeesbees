56ea9f0ea3c320c117be823ee592790d2ca677b6
paulyi326 <paul.yi326@gmail.com>
2014-06-13 09:55:32 -0700

    implemented up to bee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakeBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot = this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
